5aabb4472287fdbea96cc1eccdbb485c3bd5ee01
ASUS <hazuragaming2234@gmail.com>
2023-08-03 20:31:42 +0800

    add form validation js

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs();
+})
+
+function checkInputs() {
+    const usernameValue = username.value.trim()
+    const passwordValue = password.value.trim()
+
+    if(usernameValue === '') {
+        setErrorFor(username)
+    } else {
+
+        setSuccessFor(username)
+    }
+    if(passwordValue === '') {
+        setErrorFor(password)
+    } else {
+        setSuccessFor(password)
+    }
+}
+
+function setErrorFor(input) {
+    const warn = input.parentElement;
+    const small = warn.querySelector('small');
+    small.style.opacity = '1'
+    input.style.borderColor = 'red'
+}
+
+function setSuccessFor(input) {
+    const warn = input.parentElement;
+    const small = warn.querySelector('small');
+    small.style.opacity = '0'
+}
+
+
+
